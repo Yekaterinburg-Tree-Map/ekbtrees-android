24fea9adecad14da8ad1ac7861d88440a468f092
IlyaSecret <ilyakosyakov1@yandex.ru>
2021-11-17 18:19:29 +0500

    Changed CommentFragment name

---
+++ app/src/main/java/ru/ekbtrees/treemap/ui/treedetail/CommentFragment.kt
@@ -0,0 +1,8 @@
+package ru.ekbtrees.treemap.ui.treedetail
+
+import androidx.fragment.app.Fragment
+import ru.ekbtrees.treemap.R
+
+class CommentFragment : Fragment(R.layout.fragment_comment) {
+
+}
