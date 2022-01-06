7870a64f387868a31e9de72ebaa8249979d27516
D3rvich <ilya24.1999@gmail.com>
2022-01-06 18:58:57 +0500

    Remove upload file dto (#24)

--- app/src/main/java/ru/ekbtrees/treemap/data/files/dto/UploadFileDto.kt
+++
@@ -1,8 +0,0 @@
-package ru.ekbtrees.treemap.data.files.dto
-
-sealed class UploadFileDto {
-    class Success(val fileId: Long): UploadFileDto()
-    data class Error(
-        val exception: Exception
-    ): UploadFileDto()
-}
