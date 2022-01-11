90c6b1eb07b487f8e0205223bb58d87648bc94c7
Ivan Portnov <iportnov@naumen.ru>
2022-01-11 21:40:07 +0500

    - реализовал авторизацию и переавторизацию по логину и паролю

--- app/src/main/java/ru/ekbtrees/treemap/di/NetworkModule.kt
+++ app/src/main/java/ru/ekbtrees/treemap/di/NetworkModule.kt
@@ -8,9 +8,12 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.ekbtrees.treemap.BuildConfig
 import ru.ekbtrees.treemap.data.api.TreesApiService
+import ru.ekbtrees.treemap.data.retrofit.BaseAuthAuthenticatorInterceptor
 import ru.ekbtrees.treemap.data.retrofit.ResultAdapterFactory
 import javax.inject.Singleton
+
 
 /**
  * Dagger модуль слоя работы с сетью
@@ -19,6 +22,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     private const val BASE_URL = "https://ekb-trees-help.ru/api/"
+    private const val BASE_AUTH_URL = "https://ekb-trees-help.ru/auth/login"
 
     @Provides
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor()
@@ -27,13 +31,28 @@ object NetworkModule {
         }
 
     @Provides
+    fun provideAuthenticator(): BaseAuthAuthenticatorInterceptor =
+        BaseAuthAuthenticatorInterceptor(
+            BASE_AUTH_URL,
+            // login и password класть в local.properties
+            BuildConfig.login,
+            BuildConfig.password
+        )
+
+    @Provides
     fun provideOkHttpClient(
         httpLoggingInterceptor: HttpLoggingInterceptor,
-    ): OkHttpClient =
-        OkHttpClient
+        authenticator: BaseAuthAuthenticatorInterceptor
+    ): OkHttpClient {
+        return OkHttpClient
             .Builder()
+            .addInterceptor(authenticator)
+            .authenticator(authenticator)
+            .cookieJar(authenticator)
             .addInterceptor(httpLoggingInterceptor)
             .build()
+    }
+
 
     @Singleton
     @Provides
